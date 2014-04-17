7fff89a2e6966be1010758316fbe24ab847429d2
Tony Deng <christony610@gmail.com>
2014-04-17 11:06:20 -0400

    finished layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,51 +1,34 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-	describe "Home page" do
-		it "should have the right title" do 
-			visit '/static_pages/home' 
-			expect(page).to have_title("Micropost App | Home")
-		end 
+	subject { page }
 
-		it "should have the content 'Micropost App" do 
-			visit '/static_pages/home'
-			expect(page).to have_selector('h1', 'Micropost App')
-		end
+	describe "Home page" do
+		before { visit root_path }
+
+		it { should have_title(full_title('')) }
+		it { should_not have_title('| Home') }
+		it { should have_selector('h1','Micropost App') }
 	end
 
 	describe "Help page" do
-		it "should have the right title" do 
-			visit '/static_pages/help' 
-			expect(page).to have_title("Micropost App | Help")
-		end  
+		before { visit help_path }
 
-		it "should have the content 'Help'" do 
-			visit '/static_pages/help' 
-			expect(page).to have_selector('h1','Help')
-		end
+		it { should have_title(full_title('Help')) }
+		it { should have_selector('h1', 'Help') }
 	end
 
 	describe "About page" do
-		it "should have the right title" do 
-			visit '/static_pages/about' 
-			expect(page).to have_title("Micropost App | About")
-		end 
+		before { visit about_path }
 
-		it "should have the content 'About'" do 
-			visit '/static_pages/about'
-			expect(page).to have_selector('h1','About')
-		end 
+		it { should have_title(full_title('About')) }
+		it { should have_selector('h1', 'About') }
 	end
 
 	describe "Contact page" do
-		it "should have the right title" do 
-			visit '/static_pages/contact' 
-			expect(page).to have_title("Micropost App | Contact")
-		end  
+		before { visit contact_path }
 
-		it "should have the content 'Contact'" do 
-			visit '/static_pages/contact'
-			expect(page).to have_selector('h1','Contact')
-		end
+		it { should have_title(full_title('Contact')) }
+		it { should have_selector('h1', 'Contact') }
 	end
 end
